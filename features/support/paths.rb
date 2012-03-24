3880765e8df8f5fad1423cf6ed86661e9c17298a
Paul Wang <paul.y.wang@gmail.com>
2012-03-24 12:09:04 -0700

    fix to submit for hw4

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -15,6 +15,8 @@ module NavigationHelpers
 
     when /^the home\s?page$/
       '/movies'
+    when /^the RottenPotatoes home page$/
+      '/movies'
 	when /^the edit page for "(.*)"$/
 	  edit_movie_path(Movie.find_by_title($1))
 	when /^the details page for "(.*)"$/
